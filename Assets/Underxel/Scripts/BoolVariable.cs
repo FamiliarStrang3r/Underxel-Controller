83b826e1f45c90d70c46b40e774c3bb703b4aeca
FamiliarStrang3r <53304126+FamiliarStrang3r@users.noreply.github.com>
2020-01-26 01:45:03 +0200

    added audio

---
+++ Assets/Underxel/Scripts/BoolVariable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[Serializable]
+public struct BoolVariable
+{
+    [SerializeField] private bool variableValue;
+    public event Action<bool> OnValueChanged;
+
+    public bool Value
+    {
+        get => variableValue;
+        set
+        {
+            if (value != variableValue)
+            {
+                variableValue = value;
+                OnValueChanged?.Invoke(variableValue);
+            }
+        }
+    }
+}
+
+[CustomPropertyDrawer(typeof(BoolVariable))]
+public class BoolVariableDrawer : PropertyDrawer
+{
+    //how to make readonly: GUI.enabled = false; EditorGUI.PropertyField(); GUI.enabled = true;
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, label);
+        EditorGUI.PropertyField(position, property.FindPropertyRelative("variableValue"), GUIContent.none);
+
+        EditorGUI.EndProperty();
+    }
+}
