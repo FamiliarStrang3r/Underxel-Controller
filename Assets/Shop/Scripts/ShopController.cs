fefa1a38c89db50fd4abf88ea5fa5e8de4c0b33e
FamiliarStrang3r <53304126+FamiliarStrang3r@users.noreply.github.com>
2020-06-01 20:25:38 +0300

    shop

---
+++ Assets/Shop/Scripts/ShopController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public event Action<int> OnMoneyChanged;
+
+    [SerializeField] private RobotsDatabase robotsDatabase = null;
+    [SerializeField] private int startMoney = 100;
+    [SerializeField] private int startIndex = 0;
+
+    private RobotsDatabase database = null;
+    public RobotsDatabase RobotsDatabase
+    {
+        get
+        {
+            if (database == null)
+            {
+                //do it on awake?
+                database = Instantiate(robotsDatabase);
+            }
+
+            return database;
+        }
+    }
+
+    private int moneyAmount = 0;
+    private int selectedIndex = 0;
+
+    public int MoneyAmount
+    {
+        get => moneyAmount;
+
+        set
+        {
+            moneyAmount = value;
+            OnMoneyChanged?.Invoke(MoneyAmount);
+        }
+    }
+
+    public int SelectedIndex
+    {
+        get => selectedIndex;
+
+        set
+        {
+            selectedIndex = value;
+        }
+    }
+
+    private void Awake()
+    {
+        MoneyAmount = startMoney;
+        SelectedIndex = startIndex;
+        Load();
+    }
+
+    private void Start()
+    {
+        OnMoneyChanged?.Invoke(MoneyAmount);//invoke only at start because subscribing goes at OnEnable (after Awake, before Start)
+    }
+
+    private void Load()
+    {
+        
+    }
+
+    private void Save()
+    {
+
+    }
+}
---
+++ Assets/Shop/Scripts/ShopWindow.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopWindow : MonoBehaviour
+{
+    [SerializeField] private Text moneyLabel = null;
+    [SerializeField] private Transform elementsParent = null;
+    [SerializeField] private ShopElement elementPrefab = null;
+
+    private ShopElement[] shopElements = null;
+    private ShopController shopController = null;
+
+    private void Awake()
+    {
+        shopController = FindObjectOfType<ShopController>();
+    }
+
+    private void Start()
+    {
+        PopulateButtons();
+        UpdateElements();
+    }
+
+    private void OnEnable()
+    {
+        shopController.OnMoneyChanged += OnMoneyChanged;
+    }
+
+    private void OnDisable()
+    {
+        shopController.OnMoneyChanged -= OnMoneyChanged;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < shopElements.Length; i++)
+            {
+                var e = shopElements[i];
+                int index = e.transform.GetSiblingIndex();
+                Debug.Log(index);
+            }
+        }
+    }
+
+    private void PopulateButtons()
+    {
+        //destroy all existing
+        for (int i = 0; i < elementsParent.childCount; i++)
+        {
+            var child = elementsParent.GetChild(i);
+            Destroy(child.gameObject);
+        }
+
+        //spawn new
+        shopElements = new ShopElement[shopController.RobotsDatabase.Robots.Length];
+
+        for (int i = 0, length = shopElements.Length; i < length; i++)
+        {
+            var element = Instantiate(elementPrefab, elementsParent);
+            int k = i;
+            element.OnClick = () => OnClick(k);
+            element.name = $"Element: {i}";
+            element.transform.SetSiblingIndex(i);
+            shopElements[i] = element;
+        }
+    }
+
+    private void UpdateElements()
+    {
+        for (int i = 0, length = shopElements.Length; i < length; i++)
+        {
+            var robot = shopController.RobotsDatabase.Robots[i];
+            shopElements[i].UpdateVisual(robot, i == shopController.SelectedIndex);
+        }
+    }
+
+    public void OnClick(int buttonIndex)
+    {
+        var robot = shopController.RobotsDatabase.Robots[buttonIndex];
+
+        if (robot.isOpened)
+        {
+            shopController.SelectedIndex = buttonIndex;
+        }
+        else
+        {
+            if (shopController.MoneyAmount >= robot.costAmount)
+            {
+                shopController.MoneyAmount -= robot.costAmount;
+                robot.isOpened = true;
+            }
+            else
+            {
+                //недостаточно денег
+            }
+        }
+
+        UpdateElements();
+    }
+
+    private void OnMoneyChanged(int amount)
+    {
+        moneyLabel.text = $"У тебя {amount} деняк";
+
+        //UpdateElements();
+    }
+}
